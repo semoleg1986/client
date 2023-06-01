7969a3441eeb2df26acd649426e4aa790b5167e4
Oleg Semenov <semoleg1986@gmail.com>
2023-06-01 14:06:55 +0300

    refactor: update node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/HomePage';
 import NotFoundPage from './pages/404/NotFoundPage';
 import Layout from './components/Layout';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const App = () => {
+  const client = new ApolloClient({
+    uri: 'http://127.0.0.1:8000/graphql/',
+    cache: new InMemoryCache(),
+  });
+  
   return (
-    <Routes>
-      <Route path="*" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <ApolloProvider client={client}>
+      <Routes>
+        <Route path="*" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </ApolloProvider>
   );
 };
 export default App;
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,7 +1,46 @@
-const Home = () => {
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PRODUCTS = gql`
+  query {
+    products {
+      id
+      name
+      description
+      price
+      quantity
+    }
+  }
+`;
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+}
+
+const Home = (): JSX.Element => {
+  const { loading, error, data } = useQuery<{ products: Product[] }>(GET_PRODUCTS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <>
-      <h1>Hello</h1>
+      {data?.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <p>Quantity: {product.quantity}</p>
+        </div>
+      ))}
     </>
   );
 };
