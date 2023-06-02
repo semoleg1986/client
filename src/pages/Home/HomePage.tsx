5622682a58079c3864dd9c10c3f5545675f1842d
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-02 17:18:30 +0400

    feat: update Homepage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,40 +1,14 @@
-import { useQuery, useMutation, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { Product } from '../../types';
-import { useState } from 'react';
-import { CREATE_PRODUCT } from '../../graphql/mutation';
+import { GET_PRODUCTS } from '../..//graphql/mutation';
+import Form from '../../components/Form';
 
-const GET_PRODUCTS = gql`
-  query {
-    products {
-      id
-      name
-      description
-      price
-      quantity
-    }
-  }
-`;
 
 const Home = (): JSX.Element => {
   const { loading, error, data, refetch } = useQuery<{ products: Product[] }>(GET_PRODUCTS);
 
-  const [newProductName, setNewProductName] = useState('');
-  const [newProductDescription, setNewProductDescription] = useState('');
-  const [newProductPrice, setNewProductPrice] = useState(0);
-  const [newProductQuantity, setNewProductQuantity] = useState(0);
-
-  const [createProduct, err] = useMutation(CREATE_PRODUCT);
-
-  const handleCreateProduct = async () => {
-    console.log(newProductName)
-    await createProduct({
-      variables: {
-        name: newProductName,
-        description: newProductDescription,
-        price: newProductPrice,
-        quantity: newProductQuantity,
-      },
-    });
+  const updateProductList = () => {
+    refetch(); // Перезагрузка данных после добавления товара
   };
 
   if (loading) {
@@ -47,33 +21,9 @@ const Home = (): JSX.Element => {
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="Product Name"
-          value={newProductName}
-          onChange={(e) => setNewProductName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Product Description"
-          value={newProductDescription}
-          onChange={(e) => setNewProductDescription(e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Product Price"
-          value={newProductPrice}
-          onChange={(e) => setNewProductPrice(Number(e.target.value))}
-        />
-        <input
-          type="number"
-          placeholder="Product Quantity"
-          value={newProductQuantity}
-          onChange={(e) => setNewProductQuantity(Number(e.target.value))}
-        />
-        <button onClick={handleCreateProduct}>createProduct</button>
-      </div>
+    <div>
+      < Form updateProductList={updateProductList} />
+    </div>
       {data?.products.map((product) => (
         <div key={product.id}>
           <h2>{product.name}</h2>
