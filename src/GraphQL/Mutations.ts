0a6de22c92efdc9c47bf76b17f4ffa97cd4dd0a5
Nikolai <nikolai.ivanov3110@gmail.com>
2023-06-02 16:35:28 +0400

    feat: add Mutation file

---
+++ src/GraphQL/Mutations.ts
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_PRODUCT_MUTATION = gql`
+  mutation createProduct(
+    $name: String!
+    $description: String!
+    $price: Decimal!
+    $quantity: Int!
+  ) {
+    createProduct(
+        name: $name
+        description: $description
+        price: $price
+        quantity: $quantity
+    ) { product {
+        id
+    }
+    }
+  }
+`;
+
+
+export const GET_PRODUCTS = gql`
+  query {
+    products {
+      id
+      name
+      description
+      price
+      quantity
+    }
+  }
+`;
