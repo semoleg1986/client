6caeed144d50a33f01ef0fa077f4181596239c3d
Oleg Semenov <semoleg1986@gmail.com>
2023-06-03 17:28:53 +0300

    feat: add Cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NotFoundPage from './pages/404/NotFoundPage';
 import Layout from './components/Layout';
 import { ApolloProvider } from '@apollo/client';
 import client from './graphql/client';
-import { ROOT_PAGE, FORM_PAGE, NOT_FOUND_PAGE } from './routes';
+import { ROOT_PAGE, NOT_FOUND_PAGE, CART_PAGE } from './routes';
 
 const App = () => {
   const router = createBrowserRouter([
@@ -15,6 +15,9 @@ const App = () => {
         {
           index: true,
           element: <Home />
+        },{
+          path: CART_PAGE,
+          element: <NotFoundPage />,
         },{
           path: NOT_FOUND_PAGE,
           element: <NotFoundPage />,
