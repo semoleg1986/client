d0d487018476aee692fbb2b20c55495f175203e6
Oleg Semenov <semoleg1986@gmail.com>
2023-06-06 19:54:56 +0300

    feat: redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import cartReducer from './cartSlice';
-import cartState, { toggleCart } from './cartState';
+import cartState from './cartState';
 
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    cartstate: cartState,
-  },
-  preloadedState: {
-    cartstate: { ...cartState, isVisible: false },
-  },
+// Дополнительно импортируйте типы для Redux Persist
+import { PersistConfig } from 'redux-persist/es/types';
+import { PersistPartial } from 'redux-persist/es/persistReducer';
+
+const persistConfig: PersistConfig<RootState> = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  cartstate: cartState,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export default store;
+const persistedReducer = persistReducer<RootState, any>(persistConfig, rootReducer);
 
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
+export default { store, persistor };
