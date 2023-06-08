876be0630551c738ba09f1b5535a6e02ac3c559b
Oleg Semenov <semoleg1986@gmail.com>
2023-06-08 16:19:29 +0300

    fix: change url server

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  // uri: 'https://dj-auth-server.herokuapp.com/graphql/',
-  uri: 'http://127.0.0.1:8000/graphql/',
+  uri: 'https://dj-auth-server.herokuapp.com/graphql/',
+  // uri: 'http://127.0.0.1:8000/graphql/',
   cache: new InMemoryCache(),
 });
 
