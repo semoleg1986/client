7ccbe902a5f073c0922bfeade284b657763ebfb3
Oleg Semenov <semoleg1986@gmail.com>
2023-06-13 07:23:26 +0300

    fix: update cartSlice.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
-import cartReducer from './cartSlice';
-import cartState from './cartState';
 
 const savedAuthState = localStorage.getItem('auth');
-const savedCartState = localStorage.getItem('cart');
 
 const preloadedState = {
   auth: savedAuthState ? JSON.parse(savedAuthState) : { isAuthenticated: false },
-  cart: savedCartState ? JSON.parse(savedCartState) : { items: [] },
 };
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    cart: cartReducer,
-    cartstate: cartState,
   },
   preloadedState,
 });
@@ -23,9 +17,7 @@ const store = configureStore({
 store.subscribe(() => {
   const state = store.getState();
   const authState = state.auth;
-  // const cartState = state.cart;
   localStorage.setItem('auth', JSON.stringify(authState));
-  localStorage.setItem('cart', JSON.stringify(cartState));
 });
 
 export type RootState = ReturnType<typeof store.getState>;
